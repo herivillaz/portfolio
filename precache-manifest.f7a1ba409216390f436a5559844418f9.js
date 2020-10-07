0a958cbcaa425c66ff17be3aed63603817b6fe67
herivillaz <herivillaz@gmail.com>
2020-10-07 15:09:56 -0400

    Updates

--- precache-manifest.f7a1ba409216390f436a5559844418f9.js
+++ precache-manifest.f7a1ba409216390f436a5559844418f9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "47599cc83bfe99950d9eaee08fb7a382",
+    "revision": "4d05436af0cff8efb48335ff2c6ca69e",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "2c5ce4296ae3879068b4",
+    "revision": "479f7e4a799d66a10e5e",
     "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "03491ce2e020be58bf53",
+    "revision": "f09e7ffcad80d66ad3b1",
     "url": "/portfolio/static/css/main.43f4ddb2.chunk.css"
   },
   {
-    "revision": "2c5ce4296ae3879068b4",
-    "url": "/portfolio/static/js/2.5121294a.chunk.js"
+    "revision": "479f7e4a799d66a10e5e",
+    "url": "/portfolio/static/js/2.53cd0839.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.5121294a.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.53cd0839.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "03491ce2e020be58bf53",
-    "url": "/portfolio/static/js/main.60872ca7.chunk.js"
+    "revision": "f09e7ffcad80d66ad3b1",
+    "url": "/portfolio/static/js/main.5eb650a9.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
